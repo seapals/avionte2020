598e776809d2519b9c0f08e734bba692e3ac9d8c
Chris Straight <chris@chrisstraight.com>
2020-03-27 11:56:42 -0500

    js works

---
+++ alt.js
@@ -0,0 +1,31 @@
+function injectClass(targetType, targetName, className) {
+  var element;
+
+  switch (targetType) {
+      case "id": {
+              const element = document.getElementById(targetName);
+              for (i = 0; i < element.length; i++) {
+                  element[i].classList.add(className);
+              }
+              break;
+          }
+      case "class":
+          {
+              const element = document.getElementsByClassName(targetName);
+              for (i = 0; i < element.length; i++) {
+                  element[i].classList.add(className);
+              }
+              break;
+          }
+      case "tag":
+          {
+              const element = document.getElementsByTagName(targetName);
+              for (i = 0; i < element.length; i++) {
+                  element[i].classList.add(className);
+              }
+              break;
+          }
+  }
+}
+
+injectClass("tag", "table", "table");
