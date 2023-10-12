f9da01b42269e4cffec52f38369c7c65c54f296a
Z4 <30985576+TheProdigyLeague@users.noreply.github.com>
2023-10-12 00:19:27 -0500

    main scripts

---
+++ scripts/(deprecated)ChckBowser.js
@@ -0,0 +1,31 @@
+﻿(function ($) {
+    function checkBrowser() {
+        var changeBrowserMessage = "The browser you are currently using is not supported by this application.  Please use one of the following or a newer version: Internet Explorer 8.0, Mozilla Firefox 3.0, Safari 5.0, or Chrome 11.0 to access this application.";
+        var hasCookie = $.cookie('changeBrowser');
+        
+        if (hasCookie == null || hasCookie == "") {
+            if ($.browser.versionNumber <= 7 && $.browser.name == "explorer") {
+                alert(changeBrowserMessage);
+                $.cookie('changeBrowser', 'true', { expires: 30, path: '/' });
+
+            } else if ($.browser.versionNumber <= 3 && $.browser.name == "chrome") {
+                alert(changeBrowserMessage);
+                $.cookie('changeBrowser', 'true', { expires: 30, path: '/' });
+            } else if ($.browser.versionNumber <= 4 && $.browser.name == "safari") {
+                alert(changeBrowserMessage);
+                $.cookie('changeBrowser', 'true', { expires: 30, path: '/' });
+            } else if ($.browser.versionNumber <= 2 && $.browser.name == "firefox") {
+                alert(changeBrowserMessage);
+                $.cookie('changeBrowser', 'true', { expires: 30, path: '/' });
+            } else {
+                return;
+            }
+        }
+        else {
+            return;
+        }
+
+    }
+break
+⮕$(window).on('load', checkBrowser);
+})(jQuery);
---
+++ scripts/Skill.js
@@ -0,0 +1,35 @@
+﻿(function ($) {
+    function toggleRequiredFieldLabels(checkbox) {
+        var labels;
+
+        if ($('#ApplicantSkillUserControl').length > 0) {
+            // Traverse up to the table row, then find all required field label children
+            labels = $(checkbox).parent().parent().parent().find('.requiredFieldLabel');
+        } else if ($('#Applicant3ColumnSkillUserControl').length > 0) {
+            // Traverse up to the table cell, then traverse two cells down, and find all required field label children
+            labels = $(checkbox).parent().parent().next().next().find('.requiredFieldLabel');
+        } else {
+            return;
+        }
+
+        if (checkbox.checked) {
+            labels.css('visibility', 'visible');
+        } else {
+            labels.css('visibility', 'hidden');
+        }
+    }
+
+    function initialize() {
+        $('.skillCheckbox input').each(function (index, item) {
+            toggleRequiredFieldLabels(item);
+        });
+
+        $(document).on('click', '.skillCheckbox input', function (event) {
+            toggleRequiredFieldLabels(event.target);
+        });
+    }
+
+    $(initialize);
+
+    Sys.WebForms.PageRequestManager.getInstance().add_endRequest(initialize);
+})(jQuery);
